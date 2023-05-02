6a04d00b65c44ef0cf44dc3f13cf59cc3c81725d
Ananay Gupta <ananaygupta23@gmail.com>
2023-05-02 17:24:00 +0530

    Router Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>
+      <Navbar/>
+      <Home/>
+      <Footer/>
+    </div>,
+  },
+  {
+    path: "/register",
+    element: <Register/>,
+  },
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/login",
+    element: <Login/>,
+  },
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+       <RouterProvider router={router} />
     </div>
   );
 }
